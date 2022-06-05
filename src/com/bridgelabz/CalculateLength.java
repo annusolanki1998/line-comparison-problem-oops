b40715a0b01975c892bac0e77fda7c98215f0387
Annu <annusinghraj1998@gmail.com>
2022-06-05 17:22:15 +0530

    ADDED : Welcome to line comparision computation program !

---
+++ src/com/bridgelabz/CalculateLength.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public  class CalculateLength {
+
+    public static void welcomeMessage () {
+        System.out.println("Welcome to line comparison computation program ! ");
+    }
+
+    public static void main(String[] args) {
+        welcomeMessage();
+    }
+}
+
+
+
