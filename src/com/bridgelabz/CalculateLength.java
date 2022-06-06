8d164d3e60adbe8323c34a3f66570ed0b0e4fe5d
Annu <annusinghraj1998@gmail.com>
2022-06-06 10:25:40 +0530

    ADDED : Calculate two lines are equal or not.

--- src/com/bridgelabz/CalculateLength.java
+++ src/com/bridgelabz/CalculateLength.java
@@ -1,9 +1,9 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
+public class CalculateLength {
+    public static void calculateLengthUC2() {
 
-public  class CalculateLength {
-    public static void calculateLengthUC1 () {
         System.out.println("Calculate length between two points");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter x1 : ");
@@ -12,16 +12,33 @@ public  class CalculateLength {
         int y1 = scanner.nextInt();
         System.out.println("Enter x2 : ");
         int x2 = scanner.nextInt();
-        System.out.println("Enter y2: ");
+        System.out.println("Enter y2 : ");
         int y2 = scanner.nextInt();
-        double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        System.out.println(length);
+        int length = (int) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Length of first line is : " +length);
+
+        System.out.println("Calculate length between another two points");
+        System.out.println("Enter x3 : ");
+        int x3 = scanner.nextInt();
+        System.out.println("Enter z3 : ");
+        int z3 = scanner.nextInt();
+        System.out.println("Enter x4 : ");
+        int x4 = scanner.nextInt();
+        System.out.println("Enter z4 : ");
+        int z4 = scanner.nextInt();
+        int length1 = (int) Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(z4 - z3, 2));
+        System.out.println("Length of second line is : " +length1);
+
+        if (length == length1) {
+            System.out.println("Both lines are equal");
+        } else {
+            System.out.println("Both lines are not equal");
+        }
 
     }
-
     public static void main(String[] args) {
+        calculateLengthUC2();
 
-        calculateLengthUC1 ();
     }
 }
 
